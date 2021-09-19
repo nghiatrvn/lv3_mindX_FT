c67d52ff09e87375f1187c0d9fc1b6073661d5e7
NghiaTran <trannghiak56utc@gmail.com>
2021-09-20 04:43:13 +0700

    first commit

---
+++ NodeJS/ls1/evenloop.js
@@ -0,0 +1,7 @@
+console.log("Hi bro!");
+
+setTimeout(function callback(){
+    console.log("Nice too meet you!")
+}, 5000);
+
+console.log("What's your name?");
---
+++ NodeJS/ls1/fs_CRUD.js
@@ -0,0 +1,8 @@
+/* sử dụng fs để CRUD bảng học sinh
+{ 
+"ID":  "", 
+ "name": "",
+ "age": 23, 
+"gender": "Female",
+ "department": "History"
+ } */
