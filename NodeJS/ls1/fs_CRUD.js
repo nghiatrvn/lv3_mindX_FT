05d5f8b87f9e71c11943d162d3c7eb09a29033a6
NghiaTran <trannghiak56utc@gmail.com>
2021-09-20 11:59:52 +0700

    upload code

--- NodeJS/ls1/fs_CRUD.js
+++ NodeJS/ls1/fs_CRUD.js
@@ -1,8 +1,124 @@
-/* sử dụng fs để CRUD bảng học sinh
-{ 
-"ID":  "", 
- "name": "",
- "age": 23, 
-"gender": "Female",
- "department": "History"
- } */
+const fs = require("fs");
+
+const student = {
+  ID: 1,
+  name: "nghia",
+  age: 19,
+  gender: "male",
+  department: "History",
+};
+
+const student2 = {
+  ID: 2,
+  name: "hanh",
+  age: 19,
+  gender: "female",
+  department: "History",
+};
+
+const write = async (content) => {
+  await fs.promises
+    .write("info.json", JSON.stringify(content))
+    .catch((error) => {
+      if (error) throw error;
+    });
+};
+
+const read = async () => {
+  try {
+    const data = await fs.promises.read("./fs_CRUD.json", "utf8");
+
+    console.log("read data: ", JSON.parse(data));
+  } catch (err) {
+    throw err;
+  }
+};
+
+const create = async () => {
+  const exists = await fs.existsSync("./fs_CRUD.json");
+
+  if (!exists) {
+    write([student]);
+    console.log(" created successful");
+  } else {
+    console.log(" existed successful");
+  }
+};
+
+const check = async (studentId) => {
+  const read = await fs.promises.read("./fs_CRUD.json", "utf8");
+
+  let student = JSON.parse(read);
+
+  for (let i = 0; i < student.length; i++) {
+    if (student[i].ID === studentId) {
+      return true;
+    }
+  }
+};
+
+const addStudent = async (data) => {
+  try {
+    const read = await fs.promises.read("./fs_CRUD.json", "utf8");
+
+    let student = JSON.parse(read);
+
+    let idList = student.map((e) => e.ID);
+
+    if (idList.indexOf(data.ID) < 0) {
+      student.push(data);
+      write(student);
+    } else {
+      console.log("ID existed, try again!");
+    }
+  } catch (err) {
+    throw err;
+  }
+};
+
+const deleteStudent = async (studentId) => {
+  const read = await fs.promises.read("./fs_CRUD.json", "utf8");
+
+  if (await check(studentId)) {
+    let student = JSON.parse(read);
+
+    for (let i = 0; i < student.length; i++) {
+      if (student[i].ID === studentId) {
+        student.splice(student.indexOf(student[i]), 1);
+      }
+    }
+
+    write(student);
+  } else {
+    console.log("In studentId! Try another one");
+  }
+};
+
+const updateStudent = async (studentId, updateData) => {
+  const read = await fs.promises.read("./fs_CRUD.json", "utf8");
+
+  if (await check(studentId)) {
+    let student = JSON.parse(read);
+
+    for (let i = 0; i < student.length; i++) {
+      if (student[i].ID === studentId) {
+        student[i] = updateData;
+      }
+    }
+
+    write(student);
+  } else {
+    console.log("In studentId! Try another one");
+  }
+};
+
+const main = async () => {
+  await create();
+
+  await addStudent(student2);
+  //   await updateStudent(2, student);
+
+  await read();
+};
+
+main();
---
+++ NodeJS/ls2/index.js
@@ -0,0 +1,18 @@
+// khai báo sử dụng module HTTP
+var http = require('http');
+//Khai báo sử dụng module fs
+var fs = require('fs');
+//Khởi tạo server chạy cổng 8000
+http.createServer(function (req, res) {
+    //định dang response head trả về
+    res.writeHead('200', {'content-type': 'text/html'});
+    //đọc file code.html encode utf8
+    fs.readFile('code.html', 'utf8', function (err, data) {
+        if (err) throw err;
+        // in ra nội dung đọc được
+        res.write(data);
+        //kết thúc response
+        res.end();
+    });
+
+}).listen(8000);
